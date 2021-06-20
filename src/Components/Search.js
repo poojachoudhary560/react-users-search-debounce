3cd458ff3092c663822d2b162ff8d2c9a10d0fe8
poojachoudhary560 <poojachoudhary560@gmail.com>
2021-06-20 06:39:24 +0530

    1 file modified

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,13 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
-const Search = ({ label, searchKey, handleInputChange }) => {
+const Search = ({ label, handleInputChange }) => {
+  const [searchKey, setSearchKey] = useState('');
+
   const handleChange = e => {
-    // console.log(e.target.value);
-    handleInputChange(e.target.value);
+    console.log(e.target.value);
+    setSearchKey(e.target.value);
+
+    //handleInputChange(e.target.value);
   };
+
+  useEffect(() => {
+    let timer = setTimeout(() => {
+      handleInputChange(searchKey);
+    }, 3000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [searchKey]);
+
   return (
     <>
-      <label />
+      <label>{label}: </label>
       <input type="text" value={searchKey} onChange={handleChange} />
     </>
   );
